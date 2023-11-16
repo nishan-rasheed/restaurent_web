d360a4043e7c28ee2910adb7a53a6c2bd2c01ccf
nishan-rasheed <nishanrasheed786@gmail.com>
2023-11-16 23:53:24 +0530

    dsa

---
+++ lib/utils/dsa.dart
@@ -0,0 +1,39 @@
+// void main(){
+  
+  
+//  * * * *
+//   * * *
+//    * *
+//     *
+  
+//     for(int i = 0 ; i< 7; i++){
+//       var stars='';
+//         for(int j = (7-i); j > 1 ;j--) { 
+//            stars += '_';
+//         }
+//       for(int j = 0; j <= i ;j++){
+//            stars += '* ';
+//     }
+//       print(stars);
+//     }
+  
+ 
+//  * 
+//  * * 
+//  * * *
+//  * * * *
+  
+//    for(int i=0;i<4;i++){
+     
+//      var stars = '';
+     
+//      for(int j = 0;j<=i;j++){
+//        stars += '* ';
+//      }
+     
+//      print(stars);
+     
+//    }
+  
+  
+// }
